1c396837b3d59025f430528a44d8ae68dbfe0bd5
sunwhale <sunwhale@126.com>
2023-10-06 16:14:07 +0800

    Update optimize.py

--- scripts/optimize.py
+++ scripts/optimize.py
@@ -14,7 +14,10 @@ from scipy.optimize import fmin
 from src.pydata.utils.mechanics import load_local_status, get_elastic_limit
 
 
-def finite_element_solution(paras: list, constants: list, strain: ndarray, time: ndarray) -> tuple[ndarray, ndarray, ndarray]:
+def finite_element_solution(paras: list, constants: dict, strain: ndarray, time: ndarray) -> tuple[ndarray, ndarray, ndarray]:
+    """
+    计算耦合相场断裂的广义Maxwell模型的有限元解
+    """
     import sys
     PYFEM_PATH = r'F:\Github\pyfem\src'
     sys.path.insert(0, PYFEM_PATH)
@@ -29,18 +32,21 @@ def finite_element_solution(paras: list, constants: list, strain: ndarray, time:
     BaseIO.is_read_only = False
     nu = 0.14
     material_data = []
-    material_data.append(constants[0])
-    material_data.append(nu)
-    if len(constants) > 1:
-        m = int(len(paras))
-        for i in range(m):
+
+    material_data.append(constants['E_inf'])
+    material_data.append(constants['nu'])
+    tau_number = constants['tau_number']
+    tau = constants['tau']
+    E = constants['E']
+
+    for i in range(tau_number):
+        if len(tau) > 0:
             material_data.append(paras[i])
-            material_data.append(constants[i + 1])
-    else:
-        m = int(len(paras) / 2)
-        for i in range(m):
+            material_data.append(tau[i])
+        else:
             material_data.append(paras[2 * i])
-            material_data.append(constants[2 * i + 1])
+            material_data.append(paras[2 * i + 1])
+
     job.props.materials[0].data = material_data
     job.props.solver.total_time = total_time
     job.props.solver.max_dtime = total_time / 50.0
@@ -53,7 +59,10 @@ def finite_element_solution(paras: list, constants: list, strain: ndarray, time:
     return e11, s11, t
 
 
-def finite_element_solution_damage(paras: list, constants: list, strain: ndarray, time: ndarray) -> tuple[ndarray, ndarray, ndarray]:
+def finite_element_solution_damage(paras: list, constants: dict, strain: ndarray, time: ndarray) -> tuple[ndarray, ndarray, ndarray]:
+    """
+    计算耦合相场断裂的广义Maxwell模型的有限元解
+    """
     import sys
     PYFEM_PATH = r'F:\Github\pyfem\src'
     sys.path.insert(0, PYFEM_PATH)
@@ -66,13 +75,17 @@ def finite_element_solution_damage(paras: list, constants: list, strain: ndarray
     amplitude = [[time[i], strain[i]] for i in range(len(time))]
 
     BaseIO.is_read_only = False
+
     material_data = []
-    material_data.append(constants[0])
-    material_data.append(constants[1])
-    m = int(len(constants) / 2) - 1
-    for i in range(m):
-        material_data.append(constants[2 * (i + 1)])
-        material_data.append(constants[2 * (i + 1) + 1])
+    material_data.append(constants['E_inf'])
+    material_data.append(constants['nu'])
+    tau_number = constants['tau_number']
+    tau = constants['tau']
+    E = constants['E']
+    for i in range(tau_number):
+        material_data.append(tau[i])
+        material_data.append(E[i])
+
     job.props.materials[0].data = material_data
     job.props.materials[1].data = paras
     job.props.solver.total_time = total_time
@@ -86,52 +99,59 @@ def finite_element_solution_damage(paras: list, constants: list, strain: ndarray
     return e11, s11, t
 
 
-def analytical_tensile_solution(paras: list, constants: list, strain: ndarray, time: ndarray) -> tuple[ndarray, ndarray, ndarray]:
+def analytical_tensile_solution(paras: list, constants: dict, strain: ndarray, time: ndarray) -> tuple[ndarray, ndarray, ndarray]:
+    """
+    计算单调拉伸实验广义Maxwell模型的解析解
+    """
     t = time
     dt = np.diff(t)
     strain_rate = np.diff(strain) / np.diff(t)
     dt = np.concatenate((dt, dt[-1:]))
     strain_rate = np.concatenate((strain_rate, strain_rate[-1:]))
 
-    E_inf = constants[0]
+    E_inf = constants['E_inf']
+    nu = constants['nu']
+    tau_number = constants['tau_number']
+    tau = constants['tau']
+
     h = []
-    if len(constants) > 1:
-        m = int(len(paras))
-        for i in range(m):
-            h.append([0.])
+
+    for i in range(tau_number):
+        h.append([0.])
+        if len(tau) > 0:
             E_i = paras[i]
-            tau_i = constants[i + 1]
-            for j in range(1, len(dt)):
-                h[i].append( np.exp(-dt[j] / tau_i) * h[i][j - 1] + tau_i * E_i * (1 - np.exp(-dt[j] / tau_i)) * strain_rate[j])
-    else:
-        m = int(len(paras) / 2)
-        for i in range(m):
-            h.append([0.])
+            tau_i = tau[i]
+        else:
             E_i = paras[2 * i]
             tau_i = paras[2 * i + 1]
-            for j in range(1, len(dt)):
-                h[i].append( np.exp(-dt[j] / tau_i) * h[i][j - 1] + tau_i * E_i * (1 - np.exp(-dt[j] / tau_i)) * strain_rate[j])
+        for j in range(1, len(dt)):
+            h[i].append(np.exp(-dt[j] / tau_i) * h[i][j - 1] + tau_i * E_i * (1 - np.exp(-dt[j] / tau_i)) * strain_rate[j])
 
     ha = np.array(h).transpose()
     stress = E_inf * strain + ha.sum(axis=1)
     return strain, stress, time
 
 
-def analytical_relax_solution(paras: list, constants: list, strain: ndarray, time: ndarray) -> tuple[ndarray, ndarray, ndarray]:
-    E_inf = constants[0]
+def analytical_relax_solution(paras: list, constants: dict, strain: ndarray, time: ndarray) -> tuple[ndarray, ndarray, ndarray]:
+    """
+    计算应力松弛实验广义Maxwell模型的解析解
+    """
+    E_inf = constants['E_inf']
+    nu = constants['nu']
+    tau_number = constants['tau_number']
+    tau = constants['tau']
+
     stress = E_inf * strain
-    if len(constants) > 1:
-        m = int(len(paras))
-        for i in range(m):
+
+    for i in range(tau_number):
+        if len(tau) > 0:
             E_i = paras[i]
-            tau_i = constants[i + 1]
-            stress += E_i * np.exp(-time / tau_i) * strain
-    else:
-        m = int(len(paras) / 2)
-        for i in range(m):
+            tau_i = tau[i]
+        else:
             E_i = paras[2 * i]
             tau_i = paras[2 * i + 1]
-            stress += E_i * np.exp(-time / tau_i) * strain
+        stress += E_i * np.exp(-time / tau_i) * strain
+
     return strain, stress, time
 
 
@@ -190,20 +210,32 @@ def partial_by_elastic_limit(data: dict) -> dict:
         time = array(data[key]['Time_s'])
         strain = array(data[key]['Strain'])
         stress = array(data[key]['Stress_MPa'])
+
+        shift_indices = strain < 0.008
+        shift = len(time[shift_indices])
+
+        strain = strain[shift:] - 0.008
+        time = time[shift:] - time[shift]
+        stress = stress[shift:]
+
+        try:
+            elastic_limit, E, shift = get_elastic_limit(strain, stress, strain_start=0.005, strain_end=0.1,
+                                                        threshold=0.1)
+            elastic_indices = strain < elastic_limit[0]
+            time = time[elastic_indices]
+            strain = strain[elastic_indices]
+            stress = stress[elastic_indices]
+        except:
+            pass
+
         # try:
-        #     elastic_limit, E, shift = get_elastic_limit(strain, stress, strain_start=0.005, strain_end=0.1,
-        #                                                 threshold=10)
-        #     elastic_indices = strain < elastic_limit[0]
+        #     fracture_strain, fracture_stress = get_fracture_strain(strain, stress, -50.0)
+        #     elastic_indices = strain < (fracture_strain - 0.1)
         #     time = time[elastic_indices]
         #     strain = strain[elastic_indices]
         #     stress = stress[elastic_indices]
         # except:
         #     pass
-
-        elastic_indices = strain < 0.5
-        time = time[elastic_indices]
-        strain = strain[elastic_indices]
-        stress = stress[elastic_indices]
 
         target_rows = 100
 
@@ -227,41 +259,58 @@ def partial_by_elastic_limit(data: dict) -> dict:
     return processed_data
 
 
-def cal_tensile_cost(data: dict, paras: list, constants: list):
+def cal_tensile_cost(data: dict, paras: list, constants: dict):
+    """
+    计算单调拉伸试验的计算值与预测值的误差
+    """
     cost = 0.0
     for key in data.keys():
-        time = data[key]['Time_s']
+        time_exp = data[key]['Time_s']
         strain_exp = data[key]['Strain']
         stress_exp = data[key]['Stress_MPa']
-        f_time_stress = data[key]['f_time_stress']
-        f_time_strain = data[key]['f_time_strain']
-        stress_exp_max = max(stress_exp)
-        # strain_sim, stress_sim, time_sim = analytical_tensile_solution(paras, constants, strain_exp, time)
-        strain_sim, stress_sim, time_sim = finite_element_solution_damage(paras, constants, strain_exp, time)
-        stress_exp = f_time_stress(time_sim)
-        cost += np.sum(((stress_exp - stress_sim) / stress_exp_max) ** 2, axis=0) / len(time)
+        if constants['mode'] == 'analytical':
+            strain_sim, stress_sim, time_sim = analytical_tensile_solution(paras, constants, strain_exp, time_exp)
+        elif constants['mode'] == 'fem':
+            strain_sim, stress_sim, time_sim = finite_element_solution_damage(paras, constants, strain_exp, time_exp)
+            f_time_stress = data[key]['f_time_stress']
+            stress_exp = f_time_stress(time_sim)
+        else:
+            raise NotImplementedError
+        cost += np.sum(((stress_exp - stress_sim) / max(stress_exp)) ** 2, axis=0) / len(time_sim)
     return cost
 
 
-def cal_relax_cost(data: dict, paras: list, constants: list):
+def cal_relax_cost(data: dict, paras: list, constants: dict):
+    """
+    计算应力松弛试验的计算值与预测值的误差
+    """
     cost = 0.0
     for key in data.keys():
-        time = data[key]['Time_s']
+        time_exp = data[key]['Time_s']
         f_time_stress = data[key]['f_time_stress']
         f_time_strain = data[key]['f_time_strain']
-        time = np.linspace(time[0], time[-1], 100)
-        strain_exp = f_time_strain(time)
-        stress_exp = f_time_stress(time)
-        stress_exp_max = max(stress_exp)
-        strain_sim, stress_sim, time_sim = analytical_relax_solution(paras, constants, strain_exp, time)
-        cost += np.sum(((stress_exp - stress_sim) / stress_exp_max) ** 2, axis=0) / len(time)
+
+        # 在时间轴均匀分布应力松弛实验得到的数据
+        time_exp = np.linspace(time_exp[0], time_exp[-1], 100)
+        strain_exp = f_time_strain(time_exp)
+        stress_exp = f_time_stress(time_exp)
+
+        if constants['mode'] == 'analytical':
+            strain_sim, stress_sim, time_sim = analytical_relax_solution(paras, constants, strain_exp, time_exp)
+        elif constants['mode'] == 'fem':
+            stress_exp[0] = 0.0  # 在有限元计算应力松弛时，赋予初始应变值为0
+            strain_sim, stress_sim, time_sim = finite_element_solution_damage(paras, constants, strain_exp, time_exp)
+            stress_exp = f_time_stress(time_sim)
+        else:
+            raise NotImplementedError
+        cost += np.sum(((stress_exp - stress_sim) / max(stress_exp)) ** 2, axis=0) / len(time_exp)
     return cost
 
 
 def func(x: list):
     cost = 0.0
     cost += cal_tensile_cost(processed_tensile_data, x, constants)
-    # cost += cal_relax_cost(processed_relax_data, x, constants)
+    cost += cal_relax_cost(processed_relax_data, x, constants)
     punish = 0.0
     y = cost
     for i in range(len(x)):
@@ -275,16 +324,16 @@ if __name__ == '__main__':
     local_experiments_path = r'F:/GitHub/pydata/download/experiments'
     experiment_id = 7
     # tensile_specimen_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    tensile_specimen_ids = [1, 4, 7]
-    # tensile_specimen_ids = [11, 14, 17]
+    # tensile_specimen_ids = [1, 4, 7]
+    tensile_specimen_ids = [11, 14, 17]
     # tensile_specimen_ids = [18, 23, 24]
     tensile_experiment_data, tensile_experiment_status = get_experiment_data(local_experiments_path, experiment_id,
                                                                              tensile_specimen_ids)
     processed_tensile_data = partial_by_elastic_limit(filter_time_stress_strain(tensile_experiment_data))
 
     experiment_id = 9
-    relax_specimen_ids = [1]
-    # relax_specimen_ids = [4]
+    # relax_specimen_ids = [1]
+    relax_specimen_ids = [4]
     # relax_specimen_ids = [9]
     relax_experiment_data, relax_experiment_status = get_experiment_data(local_experiments_path, experiment_id,
                                                                          relax_specimen_ids)
@@ -293,11 +342,32 @@ if __name__ == '__main__':
     # paras_0 = [1.0, 2e-1, 1.0, 5.0e1, 1.0, 2.0e3]
     # constants = [0.97]
 
-    paras_0 = [0.01656102, 0.00236847]
+    paras_0 = [1, 1, 1]
     # constants = [0.95, 0.5, 40.0, 1500.0]
-    # constants = [0.95, 0.1, 2.0, 1000.0]
-    constants = [0.95, 0.14, 7.49243691, 0.1, 5.92043313, 2.0, 1.37908231, 1000.0]
-    paras = fmin(func, paras_0, maxiter=20, ftol=1e-4, xtol=1e-4, disp=True)
+    constants = {'E_inf': 0.95,
+                 'nu': 0.14,
+                 'mode': 'analytical',
+                 'tau_number': 3,
+                 'tau': [0.05, 40.0, 1000.0]}
+
+    # 0年
+    # paras_0 = [0.01875972, 0.00206912]
+    # paras = [0.01656102, 0.00236847]
+    # constants = [0.95, 0.14, 7.49243691, 0.1, 5.92043313, 2.0, 1.37908231, 1000.0]
+    # paras = [0.01875972, 0.00206912]
+    # constants = [0.95, 0.14, 20.47216724, 0.05, 5.05709119, 40.0, 1.2055883, 1000.0]
+
+    # 2年
+    # paras_0 = [0.01992902, 0.00181885]
+    # constants = [0.95, 0.14, 7.69765659, 0.1, 9.25720943, 2.0, 2.83577589, 1000.0]
+    # constants = [0.95, 0.14, 25.79364856, 0.05, 8.41095917, 40.0, 2.39640248, 1000.0]
+
+    # 8年
+    # paras_0 = [0.02338096, 0.00357043]
+    # constants = [0.95, 0.14, 17.65974303, 0.1, 6.20743176, 2.0, 4.36829172, 1000.0]
+    # constants = [0.95, 0.14, 43.30986079, 0.05, 7.65646085, 40.0, 4.61372817, 1000.0]
+
+    paras = fmin(func, paras_0, maxiter=5000, ftol=1e-4, xtol=1e-4, disp=True)
     print(paras)
 
     # for tau_1 in np.linspace(4e-1, 8e-1, 5):
@@ -313,8 +383,8 @@ if __name__ == '__main__':
         strain_exp = processed_tensile_data[specimen_id]['Strain']
         f_strain_stress = processed_tensile_data[specimen_id]['f_strain_stress']
         plt.plot(strain_exp, stress_exp, marker='o')
-        # strain_sim, stress_sim, time_sim = analytical_tensile_solution(paras, constants, strain_exp, time_exp)
-        strain_sim, stress_sim, time_sim = finite_element_solution_damage(paras, constants, strain_exp, time_exp)
+        strain_sim, stress_sim, time_sim = analytical_tensile_solution(paras, constants, strain_exp, time_exp)
+        # strain_sim, stress_sim, time_sim = finite_element_solution_damage(paras, constants, strain_exp, time_exp)
         plt.plot(strain_sim, stress_sim, color='red')
     plt.show()
 
@@ -324,8 +394,8 @@ if __name__ == '__main__':
         strain_exp = processed_relax_data[specimen_id]['Strain']
         f_strain_stress = processed_relax_data[specimen_id]['f_strain_stress']
         plt.plot(time_exp, stress_exp, marker='o')
-        # strain_sim, stress_sim, time_sim = analytical_relax_solution(paras, constants, strain_exp, time_exp)
-        strain_exp[0] = 0.0
-        strain_sim, stress_sim, time_sim = finite_element_solution(paras, constants, strain_exp, time_exp)
+        strain_sim, stress_sim, time_sim = analytical_relax_solution(paras, constants, strain_exp, time_exp)
+        # strain_exp[0] = 0.0
+        # strain_sim, stress_sim, time_sim = finite_element_solution_damage(paras, constants, strain_exp, time_exp)
         plt.plot(time_sim, stress_sim, color='red')
     plt.show()
